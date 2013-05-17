ffe78a00a4982b949aea8e5c6352a170277c1bad
anant jain <anant.jain@optimusinfo.com>
2013-05-17 14:07:06 +0530

    first commit

---
+++ recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: automation
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "xvfb" do
+    action [:install]
+end
